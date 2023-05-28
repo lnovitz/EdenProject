5f1275b2bfa87e76d46650a5d2e79b97424c66df
lnovitz <32498202+lnovitz@users.noreply.github.com>
2023-05-28 19:46:34 +0000

    clean up jquery injections, add working datepicker

--- controllers/create.js
+++ controllers/create.js
@@ -2,4 +2,14 @@ edenProject.controller("createController", function($scope, $location) {
   $scope.go = function(path) {
     $location.path(path);
   };
+  $("#datepicker").click(function() {
+    console.log("im picked! omgss");
+  });
+  $(document).ready(function() {
+    $("#datepicker").datepicker({
+      autoclose: true,
+      todayHighlight: true,
+    }).datepicker('update', new Date());
+
+  });
 });
