f8e9035895db2ea7097f41c2f26b6950d4371c94
lnovitz <32498202+lnovitz@users.noreply.github.com>
2023-06-03 19:58:20 +0000

    add root var for plant controller

--- controllers/plant.js
+++ controllers/plant.js
@@ -1,27 +1,29 @@
-edenProject.controller('plantController', ['$scope', '$rootScope', '$location', '$routeParams', 
+edenProject.controller('plantController', ['$scope', '$rootScope', '$location', '$routeParams',
   function($scope, $rootScope, $location, $routeParams) {
-  if ($routeParams.id) {
-    console.log("test");
-    // Edit an existing item
-    // GET to retrieve the item and load in form
-  }
-  $scope.go = function(path) {
-    $location.path(path);
-  };
+    $rootScope.isHomePage = false;
+    $rootScope.isLoggedIn = true;
 
-  $(document).ready(function() {
-    $("#datepicker").datepicker({
-      autoclose: true,
-      todayHighlight: true,
-    }).datepicker('update', new Date());
-  });
+    if ($routeParams.id) {
+      console.log("test");
+      // Edit an existing item
+      // GET to retrieve the item and load in form
+    }
+    $scope.go = function(path) {
+      $location.path(path);
+    };
 
-  // $scope.save = function() {
-  //   if ($routeParams.id) { 
-  //     // Update endpoint (PATCH)
-  //   } else {
-  //     // Create new (POST)
-  //   }
-  // }
+    $(document).ready(function() {
+      $("#datepicker").datepicker({
+        autoclose: true,
+        todayHighlight: true,
+      }).datepicker('update', new Date());
+    });
 
-}]);
+    // $scope.save = function() {
+    //   if ($routeParams.id) { 
+    //     // Update endpoint (PATCH)
+    //   } else {
+    //     // Create new (POST)
+    //   }
+    // }
+  }]);
